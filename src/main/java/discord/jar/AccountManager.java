ada64f006188da9d1cd9dea04fd70f969a979e27
xaanit <xaanit@users.noreply.github.com>
2017-04-01 10:59:15 -0500

    So many javadocs (#17)
    * Making some javadocs
    
    Made em directly on Github
    
    * Adding javadocs
    
    * More javadocs
    
    * So many javadocs
    
    I have a headache from so many javadocs
    
    * Too many javadocs
    
    * Does it ever end?

--- src/main/java/discord/jar/AccountManager.java
+++ src/main/java/discord/jar/AccountManager.java
@@ -4,13 +4,39 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public interface AccountManager {
+    /**
+    * Sets the Online Status of the account
+    *
+    * @param online Whether they are online or not
+    */
     void setOnlineStatus(boolean online);
 
+    /**
+    * Sets the display name of the account
+    *
+    * @param displayName The new Display name
+    */
     void setDisplayName(String displayName);
 
+    /**
+    * Sets the avatar of the account
+    *
+    * @param is The input stream 
+    * @throws IOException
+    */
     void setAvatar(InputStream is) throws IOException;
 
+    /**
+    * Sets the account's password
+    *
+    * @param pass The password to change it to
+    */
     void changePass(String pass);
 
+    /**
+    * Sets the account's email
+    *
+    * @param pass The email to change it to
+    */
     void changeEmail(String pass);
 }
--- src/main/java/discord/jar/DiscordAPI.java
+++ src/main/java/discord/jar/DiscordAPI.java
@@ -9,39 +9,132 @@ public interface DiscordAPI {
     DiscordAPI login(String token) throws BadUsernamePasswordException,
             DiscordFailedToConnectException;
 
+    /**
+    * Stops the DiscordAPI instance
+    */
     void stop();
 
+    /**
+    * Sets whether or not log messages are allowed
+    *
+    * @param val Whether or not they are allowed
+    */
     void setAllowLogMessages(boolean val);
 
+    /**
+    * Sets whether debug mode is on or not
+    *
+    * @param val If debug mode is on
+    */
     void setDebugMode(boolean val);
 
+    /**
+    * If the instance is in debug mode or not
+    *
+    * @return True if it is, false otherwise
+    */
     boolean isDebugMode();
-
+   
+    /**
+    * If the instance allows log messages or not
+    *
+    * @return True if it does, false otherwise
+    */
     boolean isAllowLogMessages();
 
+    /**
+    * Returns if the instance is loaded or not
+    *
+    * @return True if it is loaded, false otherwise
+    */
     boolean isLoaded();
     
+    /**
+    * If the instance is stopped or not
+    *
+    * @return True if it is, false otherwise
+    */
     boolean isStopped();
 
+    /**
+    * Attempts to join a server by ID
+    *
+    * @param id The ID to join
+    * @return True if successful, false otherwise
+    */
     Boolean joinInviteId(String id);
 
+    /**
+    * Gets the EventManager for the instance
+    *
+    * @return The EventManager
+    */
     EventManager getEventManager();
 
+    /**
+    * Gets a user by their ID
+    *
+    * @param id The ID to look for
+    * @return The User
+    */
     User getUserById(String id);
 
+    /**
+    * Gets a user by their Username
+    *
+    * @param id The name to look for
+    * @return The user
+    */
     User getUserByUsername(String id);
 
+    /**
+    * Gets a group by the ID
+    *
+    * @param id The ID to look for
+    * @return The Group
+    */
     Group getGroupById(String id);
 
+    /**
+    * Gets a server by the ID
+    *
+    * @param id The ID to look for
+    * @return The server
+    */
     Server getServerById(String id);
 
+    /**
+    * Gets every user the bot can DM
+    *
+    * @return The list of users
+    */
     List<User> getAvailableDms();
 
+    /**
+    * Gets every avaliable server
+    *
+    * @return The list of servers
+    */
     List<Server> getAvailableServers();
 
+    /**
+    * Gets the DMs of the users the bot has access to
+    *
+    * @return The map of User IDs and groups
+    */
     Map<String, Group> getUserGroups();
 
+    /**
+    * Gets the bot info
+    *
+    * @return The info
+    */
     SelfData getSelfInfo();
 
+    /**
+    * Gets the account manager for this instance
+    *
+    * @return The account manager
+    */
     AccountManager getAccountManager();
 }
