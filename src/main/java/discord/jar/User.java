7cd1fdf9919e62231d572074760ffca1242c5ca4
xaanit <xaanit@users.noreply.github.com>
2017-04-01 04:37:29 -0500

    Making some javadocs
    Made em directly on Github

--- src/main/java/discord/jar/User.java
+++ src/main/java/discord/jar/User.java
@@ -1,17 +1,52 @@
 package discord.jar;
 
 public interface User {
+    /**
+    * Gets the Group the user is in.
+    *
+    * @return The group
+    */
     Group getGroup();
 
+    /**
+    * Gets the URL to the user's avatar.
+    *
+    * @return The avatar URL
+    */
     String getAvatar();
-
+    
+    /**
+    * Gets the username of the user
+    *
+    * @return The username
+    */
     String getUsername();
 
+    /**
+    * Gets the ID of the user
+    *
+    * @return The ID
+    */
     String getId();
 
+    /**
+    * Gets the game the user is playing
+    *
+    * @return The game
+    */
     String getGame();
     
+    /**
+    * Gets the formatted mention of a user
+    *
+    * @return The formatted string
+    */
     String mention();
 
+    /**
+    * Gets the status of the user
+    *
+    * @return The OnlineStatus
+    */
     OnlineStatus getOnlineStatus();
 }
