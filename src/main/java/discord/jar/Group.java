7246aa944ecfb9199d328c2524dfa8d74bcc8d34
xaanit <xaanit@users.noreply.github.com>
2017-04-01 04:53:29 -0500

    Adding javadocs

--- src/main/java/discord/jar/Group.java
+++ src/main/java/discord/jar/Group.java
@@ -2,15 +2,47 @@ package discord.jar;
 
 
 public interface Group {
+    /**
+    * Gets the ID of the Group
+    *
+    * @return The ID
+    */
     String getId();
 
+    /**
+    * Gets the name of the Group
+    *
+    * @return The name
+    */
     String getName();
 
+    /**
+    * Gets the overlapping server the Group is in
+    *
+    * @return The server
+    */
     Server getServer();
 
+    /**
+    * Gets the message history of the group
+    *
+    * @return The MessageHistory
+    */
     MessageHistory getMessageHistory();
 
+    /**
+    * Sends a message to the Group
+    *
+    * @param message The string to send
+    * @return The Message sent
+    */
     Message sendMessage(String message);
 
+    /**
+    * Sends a message to the Group
+    *
+    * @param message The message object to send
+    * @return The Message sent
+    */
     Message sendMessage(Message message);
 }
